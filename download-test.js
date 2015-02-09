3fd74a2dde8d7b04066e7ea78ccd511af759224e
danielpgross <daniel.gross.2@gmail.com>
2015-02-08 22:35:47 -0500

    remove testing files

--- download-test.js
+++
@@ -1,6 +0,0 @@
-// Test video download and conversion to audio, outside of a server context
-var fs = require('fs');
-var ytdl = require('ytdl-core');
- 
-ytdl('https://www.youtube.com/watch?v=suY1sUYEERc', {filter: 'audioonly'})
-  .pipe(fs.createWriteStream('audio.mp3'));
--- sandbox.js
+++
@@ -1,1 +0,0 @@
-// Test youtube API access outside of a server context

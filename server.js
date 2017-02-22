28029271ad8611519cda455129c4e8d5241ff00c
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-02-22 09:58:17 +0300

    subscribe on channel

--- server.js
+++ server.js
@@ -8,17 +8,29 @@ var server = http.createServer(function(request, response) {
 	// Parse out the YT channel name
 	// Format : /[YouTube channel name]/feed.xml
 	var urlParts = request.url.split('/');
-	var username = urlParts[1];
+	var channel = urlParts[1];
 	var filename = urlParts[2];
 	var mp3FileMatch = (filename || '').match(/(.*)\.mp3$/);
 
-	if (filename == 'feed.xml') {
+	if (filename == 'feed.xml' || filename == 'user.xml') {
 		response.writeHead(200, {"Content-Type": "application/rss+xml"});
 
 		ytAudioToPodcast.getPodcastRssXmlByUsername(
-			username, 
+			channel, 
 			request.url,
-			'http://'+request.headers.host+'/'+username+'/[videoId].mp3',
+			'http://'+request.headers.host+'/'+channel+'/[videoId].mp3',
+			function(xml) {
+				response.write(xml);
+				response.end();
+			}
+		);
+  } else if (filename == 'channel.xml') {
+		response.writeHead(200, {"Content-Type": "application/rss+xml"});
+
+		ytAudioToPodcast.getPodcastRssXmlByChannelId(
+			channel, 
+			request.url,
+			'http://'+request.headers.host+'/'+channel+'/[videoId].mp3',
 			function(xml) {
 				response.write(xml);
 				response.end();
